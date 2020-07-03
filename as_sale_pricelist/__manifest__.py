5e76e4a2f136c4ebbb0bef903c1deb8304454054
phoenixsampras <phoenixsampras@gmail.com>
2020-07-03 11:20:53 -0400

    Pricelist 42.0

--- as_sale_pricelist/__manifest__.py
+++ as_sale_pricelist/__manifest__.py
@@ -11,7 +11,7 @@
     "description": """
 Lista de precios por linea de producto en ventas.
 """,    
-    "version":"13.1.9",
+    "version":"13.2.0",
     "depends" : [
         "base",
         "sale_management",
--- as_sale_pricelist/models/as_invoice.py
+++ as_sale_pricelist/models/as_invoice.py
@@ -19,11 +19,12 @@ class as_accountinvoice(models.Model):
         #_logger.debug(query_movements)
         self.env.cr.execute(query_movements)
         ids = [k for k in self.env.cr.fetchone()] 
-        line = self.env['sale.order.line'].search([('id','in',ids)],limit=1)
-        ultima_promo = self.env['tf.history.promo'].search([('sale_id','=',line.order_id.id),('product_id','=',line.product_id.id),('last_applied_promo','=',True)])
         name= ''
-        if ultima_promo:
-            name = str(ultima_promo.promo_id.id)+' : '+str(ultima_promo.promo_id.name)
-        else:
-            name =' '
+        if ids != []:
+            line = self.env['sale.order.line'].search([('id','in',ids)],limit=1)
+            ultima_promo = self.env['tf.history.promo'].search([('sale_id','=',line.order_id.id),('product_id','=',line.product_id.id),('last_applied_promo','=',True)],order="write_date desc", limit=1)
+            if ultima_promo:
+                name = str(ultima_promo.promo_id.id)+' : '+str(ultima_promo.promo_id.name)
+            else:
+                name =' '
         return name
