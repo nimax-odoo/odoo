c6f5a39573366e953f57c9d40d8f3f72c61b94f6
phoenixsampras <phoenixsampras@gmail.com>
2025-04-03 21:02:03 +0000

    Stock API 9

--- as_stock_api/__manifest__.py
+++ as_stock_api/__manifest__.py
@@ -2,7 +2,7 @@
 # © 2019 Piotr Cierkosz <info@cier.tech>
 {
     "name": "Ahorasoft Inventario API",
-    "version": "1.0.25",
+    "version": "1.0.27",
     "category": "Stock",
     "author": "Ahorasoft",
     "depends": [
