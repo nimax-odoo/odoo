ae6ac6080726540c91685ec68bd1e054f29a4f0d
Smartdoo Solutions <smartdoo.software@gmail.com>
2025-11-14 02:13:55 +0000

    NIMAX: Ajuste en listas de precio

--- sd_sales_ecommerce/controllers/website_sale.py
+++ sd_sales_ecommerce/controllers/website_sale.py
@@ -1,6 +1,7 @@
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo import fields, http, SUPERUSER_ID, tools, _
 from odoo.http import request, route
+from werkzeug.urls import url_decode, url_encode, url_parse
 
 
 class WebsiteSaleInherit(WebsiteSale):
@@ -54,3 +55,40 @@ class WebsiteSaleInherit(WebsiteSale):
                 order._validate_order()
                 order.reservar_picking_stock()
         return response
+
+
+    @route(['/shop/change_pricelist/<model("product.pricelist"):pricelist>'], type='http', auth="public", website=True, sitemap=False)
+    def pricelist_change(self, pricelist, **post):
+        website = request.env['website'].get_current_website()
+        redirect_url = request.httprequest.referrer
+        if (
+                pricelist in request.env.user.sd_pricelist_ids
+        ):
+            if redirect_url and request.website.is_view_active('website_sale.filter_products_price'):
+                decoded_url = url_parse(redirect_url)
+                args = url_decode(decoded_url.query)
+                min_price = args.get('min_price')
+                max_price = args.get('max_price')
+                if min_price or max_price:
+                    previous_price_list = request.website.pricelist_id
+                    try:
+                        min_price = float(min_price)
+                        args['min_price'] = min_price and str(
+                            previous_price_list.currency_id._convert(min_price, pricelist.currency_id, request.website.company_id, fields.Date.today(), round=False)
+                        )
+                    except (ValueError, TypeError):
+                        pass
+                    try:
+                        max_price = float(max_price)
+                        args['max_price'] = max_price and str(
+                            previous_price_list.currency_id._convert(max_price, pricelist.currency_id, request.website.company_id, fields.Date.today(), round=False)
+                        )
+                    except (ValueError, TypeError):
+                        pass
+                    redirect_url = decoded_url.replace(query=url_encode(args)).to_url()
+            request.session['website_sale_current_pl'] = pricelist.id
+            request.session['website_sale_selected_pl_id'] = pricelist.id
+            order_sudo = request.website.sale_get_order()
+            if order_sudo:
+                order_sudo._cart_update_pricelist(pricelist_id=pricelist.id)
+        return request.redirect(redirect_url or '/shop')
