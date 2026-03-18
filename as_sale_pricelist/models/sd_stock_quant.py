acccbd7c1509f97c6e25a4b271e0bef1674263c1
Catherina Gomez <smartdoosoftware@gmail.com>
2026-03-18 17:50:59 +0000

    NIMAX: ajuste en nombre de lotes por concatenacion de nombre mas pedimentos

--- as_sale_pricelist/models/sd_stock_quant.py
+++ as_sale_pricelist/models/sd_stock_quant.py
@@ -5,7 +5,7 @@ import re
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from odoo.exceptions import UserError, ValidationError
-    
+from odoo.addons.l10n_mx_edi_extended.models.account_move import CUSTOM_NUMBERS_PATTERN  
 class StockQuant(models.Model):
     _inherit="stock.quant"
 
@@ -31,3 +31,29 @@ class StockQuant(models.Model):
                 force_send=True,
             )
         return res
+
+class StockLot(models.Model):
+    _inherit = "stock.lot"
+
+    def _inverse_l10n_mx_edi_landed_cost_id(self):
+        #sustituida por necesidad de renombrar los numeros de lote porque odoo concatena con pedimentos causando conflicto.
+        for lot in self:
+            if not lot.product_id.l10n_mx_edi_can_use_customs_invoicing:
+                continue
+            split_name = lot.name.rsplit("/", 1)
+            # If we already have a customs on the name we take only what is not, else we just take the whole name
+            prefix_name = split_name[0].strip() if len(split_name) > 1 and CUSTOM_NUMBERS_PATTERN.match(split_name[1].strip()) else lot.name
+
+            if prefix_name and lot.l10n_mx_edi_customs_number:
+                lot.name = f"{prefix_name}"
+                lot.ref = f"{lot.l10n_mx_edi_customs_number}"
+            else:
+                lot.name = prefix_name or lot.l10n_mx_edi_customs_number
+                lot.ref = f"{lot.l10n_mx_edi_customs_number}"
+
+    def _get_recompute_name(self):
+        for lot in self:
+            nombre = lot.name.split(' / ')
+            if len(nombre) > 1:
+                lot.name = nombre[0]
+                lot.ref = f"{lot.l10n_mx_edi_customs_number}"
